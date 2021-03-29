4820a22a391620a64a601f78a37ab5872ad31895
Luca Vlad <vladluca50@gmail.com>
2021-03-30 00:39:15 +0300

    Initial Push

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/burger")
+def burger():
+    return render_template("index.html", model_path="/models/burger.glb")
+
+
+@app.route("/burger2")
+def burger2():
+    return render_template("index.html", model_path="/models/burger2.glb")
+
+
+@app.route("/panda")
+def panda():
+    return render_template("index.html", model_path="/models/pandas.glb")
+
+
+if __name__ == "__main__":
+    app.run()
